e46ed625dd8863cc781617cae77e56f79ffd6533
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-02-06 12:46:07 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -9,6 +9,7 @@ def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
   
-  BASE_HOA[:chipmunks][2] = "RYAN"
-  puts BASE_HOA[:chipmunks][2]
+  BASE_HOA[show] << name
+  BASE_HOA[show]
+  
 end
